a824404c52bda4a42077986e606346218b8a1bd3
lx1993333 <lx1993333@sina.com>
2016-04-27 13:24:21 -0700

    enemy 0.1

---
+++ Slime/Assets/Script/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Enemy : MonoBehaviour {
+
+
+	private CircleCollider2D circleCollider;
+	private Rigidbody2D rb2d;
+	public float inverseMoveTime = 2 ;
+	protected void Move(int xDir, int yDir)
+	{
+		Vector2 start = transform.position;
+		Vector2 end = start + new Vector2 (xDir, yDir);
+
+		StartCoroutine (SmoothMovement (end));
+
+	
+	}
+
+
+	protected virtual void Start() 
+	{
+		circleCollider = GetComponent<CircleCollider2D> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+	
+	}
+
+	protected IEnumerator SmoothMovement (Vector3 end)
+	{
+		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+		while (sqrRemainingDistance > float.Epsilon) 
+		{
+			Vector3 newPosition = Vector3.MoveTowards (rb2d.position, end, inverseMoveTime * Time.deltaTime);
+			rb2d.MovePosition (newPosition);
+			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+			yield return null;
+		}
+	}
+}
