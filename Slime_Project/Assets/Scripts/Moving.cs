b60f8da7f293ad61a60aa452c63d88d8071b9342
Jiafengchi <jiafeng@128-223-223-252-wireless.uoregon.edu>
2016-05-12 17:50:35 -0700

    title menue

---
+++ Slime_Project/Assets/Scripts/Moving.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+
+	public float speed;
+
+	void Start ()
+	{
+		//GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	
+	}
+	void Update(){
+		//transform.position += new Vector2 (Input.GetAxis ("Horizontal;"), 0) * speed * Time.deltaTime;
+
+		float x = Input.GetAxis ("Horizontal");
+
+		transform.position = new Vector2 (transform.position.x - speed * Time.deltaTime, transform.position.y);
+	}
+}
