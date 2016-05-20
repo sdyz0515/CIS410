1f966ac90cbe9c625b6a2484fb00b43a2f12ddb1
lx1993333 <lx1993333@sina.com>
2016-05-19 17:33:32 -0700

    boss added

---
+++ Slime_Project/Assets/Scripts/BossController.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class BossController: Enemy {
+
+	private Transform target;
+	private float Hp = 15.0f;
+	public static bool facingRight = false;
+	private bool faceright = false;
+	private string status;
+	public SpriteRenderer renderer;
+
+	protected override void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		bolt_num = 1;
+		base.Start ();
+	}
+
+	void Flip () 
+	{
+		faceright = !faceright;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		inverseMoveTime = 2;
+	}
+
+	void FixedUpdate () {
+		print (Hp);
+		float x = 0.0f;
+		float offset = player.transform.position.x - transform.position.x;
+
+		if (offset > float.Epsilon && !faceright) 
+			Flip ();
+		else if (offset < float.Epsilon && faceright)
+			Flip ();
+
+		if (Mathf.Abs (target.position.x - transform.position.x) > float.Epsilon)
+			x = target.position.x > transform.position.x ? 1 : -1;
+		Move (x, 0);
+
+		switch (status){
+
+		case "burn":
+			Hp -= 0.01f;
+			if (Hp <= 0) {
+				GameObject deadcopy = Instantiate (dead, transform.position, transform.rotation) as GameObject;
+				Destroy (deadcopy, 1);
+				Destroy (gameObject);
+			} 
+			break;
+
+		default:
+			break;
+		}
+
+		facingRight = faceright;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{   
+		switch (other.tag) {
+
+		case "Bolt":
+			Hp-= 1.0f;
+			Destroy (other.gameObject);
+			SoundManager.instance.PlaySingle (enemyHitSound);
+			if (Hp  <= 0.0f) {
+				print ("dead");
+				GameObject deadcopy = Instantiate (dead, transform.position, transform.rotation) as GameObject;
+				Destroy (deadcopy, 1);
+				Destroy (gameObject);
+			} else {
+				GameObject hitcopy = Instantiate (hit, transform.position, transform.rotation) as GameObject;
+				Destroy (hitcopy, 0.5f);
+			}
+			break;
+
+
+		case "Fire_Bolt":
+			Hp-= 1.0f;
+			Destroy (other.gameObject);
+			SoundManager.instance.PlaySingle (enemyHitSound);
+			status = "burn";
+			renderer.color = Color.red;
+			if (Hp  <= 0.0f) {
+				GameObject deadcopy = Instantiate (dead, transform.position, transform.rotation) as GameObject;
+				Destroy (deadcopy, 1);
+				Destroy (gameObject);
+			} else {
+				GameObject hitcopy = Instantiate (hit, transform.position, transform.rotation) as GameObject;
+				Destroy (hitcopy, 0.5f);
+			}
+
+			break;
+
+		case "Ice_Bolt":
+			Hp -= 0.5f;
+			Destroy (other.gameObject);
+			SoundManager.instance.PlaySingle (enemyHitSound);
+			status = "iced";
+			renderer.color = Color.blue;
+			inverseMoveTime -= 0.5f;
+			if (inverseMoveTime <= 0.0f)
+				inverseMoveTime = 0.0f;
+			if (Hp  <= 0.0f) {
+				GameObject deadcopy = Instantiate (dead, transform.position, transform.rotation) as GameObject;
+				Destroy (deadcopy, 1);
+				Destroy (gameObject);
+			} else {
+				GameObject hitcopy = Instantiate (hit, transform.position, transform.rotation) as GameObject;
+				Destroy (hitcopy, 0.5f);
+			}
+
+			break;
+	
+		case "LeftBounder":
+			if(!facingRight)
+				inverseMoveTime = 0;
+			break;
+		
+		case "RightBounder":
+			if(facingRight)
+				inverseMoveTime = 0;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+
+
+}
+
+
+
+
---
+++ Slime_Project/Assets/Scripts/Move_boss_bolt.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_boss_bolt : MonoBehaviour {
+
+	private bool faceright;
+	public float speed;
+	public float range = 13.0f;
+	private int x_direction = 1;
+	private Rigidbody2D rd2d;
+	private Vector2 inti_postion;
+
+	void Start ()
+	{  	
+		inti_postion = transform.position;
+
+		faceright = BossController.facingRight;
+		if (faceright)
+			x_direction = 1;
+		else
+			x_direction = -1;
+		rd2d = GetComponent<Rigidbody2D> ();
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= x_direction;
+		transform.localScale = theScale;
+
+		Vector2 movement = new Vector2 (x_direction * speed, rd2d.velocity.y);
+		rd2d.velocity = movement;
+	}
+
+	void Update() 
+	{	
+		print(inti_postion.x- transform.position.x);
+		faceright = BossController.facingRight;
+		if ((inti_postion.x - transform.position.x) * (inti_postion.x - transform.position.x) >= range * range) {
+			Destroy (gameObject);
+		}
+	}
+		
+
+
+
+
+}
